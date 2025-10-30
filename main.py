345566065039b8042ea69622d79510b0f4809c89
Paulo Daniel <92828125+kampello@users.noreply.github.com>
2025-10-30 12:27:27 +0000

    estrotura main adicionada

---
+++ main.py
@@ -0,0 +1,25 @@
+# GreenLink - Sistema de Gestão de Vegetais com SQLite
+import sqlite3
+
+def conectar():
+    return sqlite3.connect("data/greenlink.db")
+
+def login():
+    print("=== LOGIN ===")
+    nome = input("Nome: ")
+    senha = input("Senha: ")
+
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute("SELECT tipo FROM utilizadores WHERE nome=? AND senha=?", (nome, senha))
+    user = cursor.fetchone()
+    conn.close()
+
+    if user:
+        print(f"Bem-vindo, {nome}! Tipo de utilizador: {user[0].capitalize()}")
+    else:
+        print("Credenciais inválidas.")
+
+if __name__ == "__main__":
+    print("=== GreenLink ===")
+    login()
