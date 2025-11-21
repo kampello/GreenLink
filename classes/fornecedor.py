a8e16129260259f5d8a7f69d73717db90f209c39
ricardo <ricardoaascunha@gmail.com>
2025-11-21 19:57:43 +0000

    Envio de tickets ao admin

--- classes/fornecedor.py
+++ classes/fornecedor.py
@@ -1,5 +1,5 @@
 from tools.fornecedor_tools.stock import ver_stock, atualizar_stock
-from tools.fornecedor_tools.comunicacao import enviar_mensagem, ver_mensagens
+from tools.fornecedor_tools.comunicacao import enviar_mensagem, ver_mensagens,abrir_ticket_produto
 
 class Fornecedor:
     def __init__(self, db_connection, nome):
@@ -31,7 +31,7 @@ class Fornecedor:
             elif opcao == "5":
                 ver_mensagens(self.db, self.nome)
             elif opcao == "6":
-                print("→ Função abrir ticket ainda não implementada")
+                abrir_ticket_produto(self.db, self.nome)
             elif opcao == "0":
                 print("Saindo do painel do fornecedor...")
                 break
