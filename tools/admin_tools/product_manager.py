51a0216f1eda31f2cc372151f1701d6cc2a1c491
Gonçalo Augusto <goncaloaugusto2006@hotmail.com>
2025-11-21 13:42:00 +0000

    FIX: correção de erro do ticket

--- tools/admin_tools/product_manager.py
+++ tools/admin_tools/product_manager.py
@@ -113,6 +113,7 @@ def aprovar_ticket(db):
     #Obter fornecedor_id
     cursor.execute("SELECT id FROM fornecedores WHERE nome = ?", (fornecedor,))
     res = cursor.fetchone()
+    if not res:
     if escolha == "A":
         cursor.execute("INSERT INTO produtos (nome, preco, stock) VALUES (?, ?, ?)", (produto, preco, stock))
         cursor.execute("UPDATE tickets_produto SET status='feito' WHERE id=?", (ticket_id,))
